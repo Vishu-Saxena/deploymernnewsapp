5f49f030e9876cbc7cc388aad9cafcd9c97fecf4
Vishu_Saxena <vishusaxena3110@gmail.com>
2023-12-15 01:31:55 +0530

    upadted

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dbConnect = require('./DBconnection/dbconnect');
+const dotenv = require('dotenv');
+const authRout = require('./router/authRoute');
+const newsRout = require('./router/newsRoute');
+const videoRoute = require('./router/videoRoute');
+const cors = require('cors');
+const app = express();
+// app.use(cors());
+app.use(cors());
+dotenv.config();
+
+app.get('/' , (req , res)=>{
+    res.json('mansi saxena');
+})
+app.use(express.json());// req.body ke ander se data json format mai ata h , agr ye function use nhi kiya toh error ayega
+
+app.use("/api/v1/auth" , authRout);
+app.use("/api/v1/news" , newsRout);
+app.use("/api/v1/video" , videoRoute);
+
+dbConnect();
+
+// server set-up
+const port = process.env.PORT || 3000;
+app.listen( port ,()=>{
+    console.log(`server running over port ${port}`);
+});
