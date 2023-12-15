f3d0abb74e583bf11046d4444ce28312812e47f3
Vishu_Saxena <vishusaxena3110@gmail.com>
2023-12-16 01:45:17 +0530

    Update NewsHome.js

--- client/src/component/NewsHome.js
+++ client/src/component/NewsHome.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import NewsCard2 from './subcomponent/NewsCard2';
 import { useNewsContex } from '../context/AllnewsContex';
+import VideoCard from './subcomponent/VideoCard';
 
 const NewsHome = () => {
-  const {sports , science , entertainment , business , health , global} = useNewsContex();
+  const {sports , science , entertainment , business , videos , global} = useNewsContex();
     return (
         <>
         <div className='container-fluid mb-3'>
         <div className="row mt-3" >
           <h1 style={{"color" : '#ee6c02'}} > <span className='text-dark'> Spo</span>rts</h1>
-          {sports&& sports.slice(0,8).map((news , indx)=>{
+          {sports&& sports.slice(0,4).map((news , indx)=>{
             return  <NewsCard2 key={indx} news = {news}/>
           }) }
           </div>
         <div className="row mt-3">
           <h1 style={{"color" : '#ee6c02'}}> <span className='text-dark'>Busi</span>ness</h1>
-          {business && business.slice(0,8).map((news , indx)=>{
+          {business && business.slice(0,4).map((news , indx)=>{
             return  <NewsCard2 key={indx} news = {news}/>
           }) }
           </div>
@@ -33,8 +34,14 @@ const NewsHome = () => {
           </div>
         <div className="row mt-3">
           <h1 style={{"color" : '#ee6c02'}}> <span className='text-dark'>U</span>SA</h1>
-          { global && global.slice(0,8).map((news , indx)=>{
+          { global && global.slice(0,4).map((news , indx)=>{
             return  <NewsCard2 key={indx} news = {news}/>
+          }) }
+          </div>
+        <div className="row mt-3">
+          <h1 style={{"color" : '#ee6c02'}}> <span className='text-dark'>Vid</span>eos</h1>
+          { videos && videos.slice(0,4).map((ele , indx)=>{
+            return  <VideoCard key={indx} vdo={ele}/>
           }) }
           </div>
         </div>
